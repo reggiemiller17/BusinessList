bada6ad05d9bada38202915fd419e223ee858df5
reggiemiller17 <regioda31@gmail.com>
2021-10-29 12:01:09 -0400

    Updated db URI to work with MongoDB Atlas

--- server/config/db.js
+++ server/config/db.js
@@ -2,5 +2,6 @@
 
 module.exports = 
 {
-   "URI": "mongodb://localhost/contact_list"
+   //"URI": "mongodb://localhost/contact_list"
+   "URI": "mongodb+srv://Reggie:YWapILhxSgpQ9EaD@mongodbserver.ngq90.mongodb.net/contact_list?retryWrites=true&w=majority"
 }
